ec0f91298c3281f8ba0f3722fca1ba3036821a05
AkiraFukushima <h3.poteto@gmail.com>
2017-11-13 17:57:20 +0900

    Update signal handler

--- main.go
+++ main.go
@@ -16,26 +16,47 @@ func main() {
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
 
-	exitChan := make(chan int)
-	go func() {
+	stopChan := make(chan int, 1)
+
+	go func(stopChan chan int) {
+		count := 0
 		for {
-			s := <-signalChan
-			switch s {
-			case syscall.SIGHUP, syscall.SIGTERM, syscall.SIGKILL:
-				log.Println("exit")
-				exitChan <- 0
+			select {
+			case _ = <-stopChan:
+				log.Println("loop stopped")
+				break
 			default:
-				exitChan <- 1
+				time.Sleep(1 * time.Second)
+				count++
+				log.Println("count", count)
 			}
 		}
-	}()
-	go func() {
-		for {
-			time.Sleep(1 * time.Second)
-			log.Println("count")
-		}
-	}()
+	}(stopChan)
 
-	code := <-exitChan
-	os.Exit(code)
+	for {
+		select {
+		case ch := <-signalChan:
+			stopChan <- 1
+			code := catchSig(ch)
+			os.Exit(code)
+		}
+	}
+
+}
+
+func catchSig(sig os.Signal) int {
+	switch sig {
+	case syscall.SIGHUP:
+		log.Println("SIGHUP Happend! ", sig)
+		return 0
+	case syscall.SIGTERM:
+		log.Println("SIGTERM Happend! ", sig)
+		return 0
+	case syscall.SIGKILL:
+		log.Println("SIGKILL Happend! ", sig)
+		return 0
+	default:
+		log.Println("Other singal Happend! ", sig)
+		return 1
+	}
 }
